1d0d81d19b07041b7eb71c73b8b9e5a9dd0ec930
Takuya0202 <shimozono.t.24kdgn@gmail.com>
2025-07-11 18:48:36 +0900

    修正

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Takuya0202.github.io">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </StrictMode>,
